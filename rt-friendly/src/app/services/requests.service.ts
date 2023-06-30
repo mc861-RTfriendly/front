4a4dd815d81bcef18fa69e664bef85d068633c2c
Ivan Korsakov / Colina <ivan.korsakov@awarelog.com>
2023-06-30 02:27:03 -0300

    Fixing upload file

--- rt-friendly/src/app/services/requests.service.ts
+++ rt-friendly/src/app/services/requests.service.ts
@@ -23,7 +23,21 @@ export class RequestsService {
     return this.http.get('/api/queues', options);
   }
 
-  createTicket(body: any) {
+  createTicket(body: any, file: File = null) {
+    const formData: FormData = new FormData();
+  
+    formData.append('data', body);
+
+    if (file) {
+      formData.append('file', file);
+    }
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'multipart/form-data',
+      })
+    };
+
     return this.http.post('/api/ticket', body);
   }
 }
